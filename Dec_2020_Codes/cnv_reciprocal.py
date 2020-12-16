9a046f2dd7d2cc9b8f39e56a5cdc91efa2688d50
Aiswarya P.S <69599173+ashpsnair@users.noreply.github.com>
2020-12-16 17:36:56 +0530

    Updated cnv_reciprocal.py

--- Dec_2020_Codes/cnv_reciprocal.py
+++ Dec_2020_Codes/cnv_reciprocal.py
@@ -1,6 +1,5 @@
 import pandas as pd
-bam= pd.read_table("C:/Users/ash24/Downloads/IN-423-TJZD-F2-RE.bam_CNVs")
-
+bam=pd.read_table("C:/Users/ash24/Downloads/IN-423-TJZD-F2-RE.bam",names=["chr", "strt", "end", "v1","v2"])
 for i in range(len(bam)):
     if bam.strt[i]>bam.end[i]:
         start=bam.strt[i]
@@ -8,4 +7,4 @@ for i in range(len(bam)):
         bam.strt[i]=end
         bam.end[i]=start
 
-bam.to_csv("C:/Users/ash24/Downloads/IN-423-TJZD-F2-RE.output.txt", sep="\t",index=False)
+bam.to_csv("C:/Users/ash24/Downloads/IN-423-TJZD-F2-RE.output.txt", sep="\t",index=False, header=False)
