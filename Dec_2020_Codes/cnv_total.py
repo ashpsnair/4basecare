a4fb455606106f46bd48a7e1b9e548a22c0e45a9
Aiswarya P.S <69599173+ashpsnair@users.noreply.github.com>
2021-01-21 17:35:32 +0530

    Update cnv_total.py

--- Dec_2020_Codes/cnv_total.py
+++ Dec_2020_Codes/cnv_total.py
@@ -20,14 +20,19 @@ print("No. of modifications: " + str(len(bam[bam.c_start>bam.c_end])))
 genes= pd.read_table(genes_input,names=["chr", "g_start", "g_end", "gene"])
 
 df=pd.DataFrame(columns=['chr_no','start','end','predicted_copy_number','consequences','gene'])
+
 for i in range(len(genes)):
     g_start=genes.g_start[i]
     g_end=genes.g_end[i]
+    gchr= genes.chr.loc[i]
     for j in range(len(bam)):
         c_start=bam.c_start[j]
         c_end=bam.c_end[j]
-        if (g_start<c_start) & (c_end<g_end):
+        bchr=bam.chr.loc[j]
+        
+        if (g_start<c_start) & (c_end<g_end)& (gchr==bchr):
+            print(j)
             df = df.append({'chr_no': bam.iloc[j,0], 'start': str(bam.iloc[j,1]),'end': str(bam.iloc[j,2]),'predicted_copy_number': bam.iloc[j,3], 'consequences': bam.iloc[j,4],'gene':genes.iloc[i,3] }, ignore_index=True)
-    
+
 df.to_csv(final_output,sep="\t",index=False)
 
